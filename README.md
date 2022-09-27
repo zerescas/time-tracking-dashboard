be5d526bae3889a59119d08fcea62eeb7d0b2cbc
Alexandr <77718874+zerescas@users.noreply.github.com>
2022-09-27 15:34:08 +0900

    Stable implementation

---
+++ README.md
@@ -0,0 +1,69 @@
+# Frontend Mentor - Time tracking dashboard solution
+
+This is a solution to the [Time tracking dashboard challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/time-tracking-dashboard-UIQ7167Jw). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Switch between viewing Daily, Weekly, and Monthly stats
+
+### Screenshot
+
+![](images/screenshots/screenshot-desktop.jpeg)<br>
+<strong>Desktop</strong>
+
+<details>
+    <summary>Laptop</summary>
+  
+![](images/screenshots/screenshot-laptop.jpeg)<br>
+<strong>Laptop</strong>
+</details>
+
+<details>
+    <summary>Tablet</summary>
+  
+![](images/screenshots/screenshot-tablet.jpeg)<br>
+<strong>Laptop</strong>
+</details>
+
+<br>
+
+![](images/screenshots/screenshot-mobile_1.jpg)
+![](images/screenshots/screenshot-mobile_2.jpg)<br>
+<strong>Mobile</strong>
+
+### Links
+
+- Live Site URL: [...](...)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties, flexbox and grid
+
+### What I learned
+
+Improve my HTML / CSS knowledges.
+
+## Author
+
+- Twitter - [https://twitter.com/zerescas](https://twitter.com/zerescas)
+- Telegram - [https://t.me/zerescas](https://t.me/zerescas)
---
+++ js/index.js
@@ -0,0 +1,82 @@
+let timeTrackingDashboard;
+let prevTimeframeNames;
+let statsData;
+
+loadFile();
+
+async function loadFile() {
+  const response = await fetch("data.json");
+  statsData = await response.json();
+  init();
+}
+
+function init() {
+  timeTrackingDashboard = $(".time-tracking-dashboard");
+  prevTimeframeNames = {
+    daily: "Day",
+    weekly: "Week",
+    monthly: "Month"
+  }
+
+  // Easter egg
+  $(".user-card__avatar").on("click", function () {
+    $(".user-card__avatar").attr("src", "images/coder-avatar.png");
+    $(".user-card__user-name").text("zerescas");
+  });
+
+  // Add event listener to each timeframe radio button 
+  $(".user-card__timeframe-btn input").on("click", function () {
+    const timeframe = $(this).attr("value");
+    createStatCards(timeframe);
+  });
+
+  // Select weekly timeframe by default
+  $("#weekly-timeframe-btn").click();
+}
+
+function createStatCards(timeframe) {
+  // Remove old statistic cards
+  $(".stat-card").remove();
+
+  statsData.forEach(function (statData) {
+    processData(statData, timeframe);
+  })
+}
+
+function processData(statData, timeframe) {
+  const timeframeData = statData.timeframes[timeframe];
+  const statName = toLowerCase(statData.title);
+  const prevTimeframeName = getPrevTimeframeName(timeframe);
+
+  timeTrackingDashboard.append(`
+        <div class="stat-card">
+          <div class="stat-card__bg stat-card__bg--${statName}">
+            <img class="stat-card__icon" src="/images/icon-${statName}.svg" alt="${statName}-icon">
+          </div>
+
+          <div class="stat-card__bottom-content">
+
+            <div class="stat-card__titlebar">
+              <div class="stat_card__header">${statData.title}</div>
+              <div class="stat-card__action-btn">
+                <img src="/images/icon-ellipsis.svg" alt="ellipsis-icon">
+              </div>
+            </div>
+
+            <div class="stat-card__data-container">
+              <div class="stat-card__current-data">${timeframeData.current}hrs</div>
+              <div class="stat-card__prev-data">Last ${prevTimeframeName} - ${timeframeData.previous}hrs</div>
+            </div>
+
+          </div>
+        </div>
+`);
+}
+
+function toLowerCase(string) {
+  return string.toLowerCase().replace(" ", "-");
+}
+
+function getPrevTimeframeName(timeframe) {
+  return prevTimeframeNames[timeframe];
+}
